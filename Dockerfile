d18ce45af08a3ba6445c393fd24eed6e263bb258
Evan Harmon <evan.p.harmon@gmail.com>
2018-03-25 17:44:58 -0400

    fix github groovy

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ USER ${user}
 
 COPY groovy/init.groovy /usr/share/jenkins/ref/init.groovy.d/init.groovy
 COPY groovy/security.groovy /usr/share/jenkins/ref/init.groovy.d/security.groovy
-# COPY groovy/github.groovy /usr/share/jenkins/ref/init.groovy.d/github.groovy
+COPY groovy/github.groovy /usr/share/jenkins/ref/init.groovy.d/github.groovy
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
---
+++ groovy/github.groovy
@@ -0,0 +1,42 @@
+#!groovy
+import jenkins.model.*
+import hudson.security.*
+import hudson.util.Secret
+import org.jenkinsci.plugins.GithubSecurityRealm
+import org.jenkinsci.plugins.plaincredentials.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.*;
+
+def env = System.getenv()
+
+String user = env['GITHUB_USER']
+String token = env['GITHUB_TOKEN']
+String email = env['GITHUB_EMAIL']
+
+def instance = Jenkins.getInstance()
+
+def desc = instance.getDescriptor("hudson.plugins.git.GitSCM")
+
+desc.setGlobalConfigName(user)
+desc.setGlobalConfigEmail(email)
+desc.save()
+
+// https://support.cloudbees.com/hc/en-us/articles/217708168-create-credentials-from-groovy
+String id = java.util.UUID.randomUUID().toString()
+secretText = new StringCredentialsImpl(CredentialsScope.GLOBAL,id, "github jenkins token", Secret.fromString(token))
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), secretText)
+
+String githubWebUri = 'https://github.com'
+String githubApiUri = 'https://api.github.com'
+String clientID = user
+String clientSecret = token
+String oauthScopes = 'read:org'
+SecurityRealm github_realm = new GithubSecurityRealm(githubWebUri, githubApiUri, clientID, clientSecret, oauthScopes)
+
+
+if (!github_realm.equals(instance.getSecurityRealm())) {
+  instance.setSecurityRealm(github_realm)
+  instance.save()
+}
